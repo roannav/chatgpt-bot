5e388576fe47a01df98cad5eedab66b8564acec3
Roanna Victorio <roannav@users.noreply.github.com>
2024-02-24 10:59:30 -0800

    initial test

---
+++ openai-test-deprecated.py
@@ -0,0 +1,43 @@
+import os
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+# Code of your application, which uses environment variables (e.g. from `os.environ` or
+# `os.getenv`) as if they came from the actual environment.
+ 
+openai.api_key = os.getenv("OPEN_API_KEY")
+
+#client = openai()
+# defaults to getting the key using os.environ.get("OPENAI_API_KEY")
+# if you saved the key under a different environment variable name, you can do something like:
+# client = OpenAI(
+#   api_key=os.environ.get("CUSTOM_ENV_NAME"),
+# )
+
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+  ]
+)
+'''
+Traceback (most recent call last):
+  File "openai-test.py", line 19, in <module>
+    completion = openai.ChatCompletion.create(
+  File "/home/roanna/gh/chatgpt-interview-bot/venv/lib/python3.8/site-packages/openai/lib/_old_api.py", line 39, in __call__
+    raise APIRemovedInV1(symbol=self._symbol)
+openai.lib._old_api.APIRemovedInV1: 
+
+You tried to access openai.ChatCompletion, but this is no longer supported in openai>=1.0.0 - see the README at https://github.com/openai/openai-python for the API.
+
+You can run `openai migrate` to automatically upgrade your codebase to use the 1.0.0 interface. 
+
+Alternatively, you can pin your installation to the old version, e.g. `pip install openai==0.28`
+
+A detailed migration guide is available here: https://github.com/openai/openai-python/discussions/742
+'''
+
+print(completion.choices[0].message)
